90ffa4af64794f73b6df2a72426e5c8c2878fed5
lfuis201 <lmamania@ulasalle.edu.pe>
2022-09-29 19:31:19 -0500

    funciona con hamachi entre 2 ordenadores

--- src/Ejemplo1Emisor.java
+++ src/Ejemplo1Emisor.java
@@ -5,35 +5,58 @@ import java.io.*;
 public class Ejemplo1Emisor {
 
 	public static void main (String[] args) {
+		
+		//Creamos el scaner para recibir el input del usuario
 		Scanner sc = new Scanner(System.in);
+		
+		//creamos las variables: ip, puerto y mensaje
+		
 		String ip;
 		int puerto;
 		String mensaje;
 		
+		//Pedimos al usuario que igrese su mensaje
 		System.out.println ("Ingrese el mensaje: ");
 		
+		//recibimos el mensaje del usuario
 		mensaje = sc.nextLine();
 		
+		//Pedimos al usuario que igrese la ip del servidor
 		System.out.println ("Ingrese la ip: ");
 		
+		//recibimos la ip del servidor
 		ip = sc.nextLine();
 		
+		//Pedimos al usuario que igrese el puerto del servidor
 		System.out.println ("Ingrese el puerto: ");
 		
+		//recibimos el puerto el servidor
 		puerto = sc.nextInt();
-		
-
+	
 	
 		try{
 			
-
+			
+			//Determinamos la ip del servidor con el metodo "getbyname"
 			InetAddress maRe = InetAddress.getByName(ip);
+			
+			// Creamos un nuevo datagram
 			DatagramSocket miSocket = new DatagramSocket();
+			
+			//Determinamos el mensaje 
 			byte[] almacen = mensaje.getBytes();
+			
+			// Creamos un nuevo datagram con los parametros establecidos previamente
 			DatagramPacket datagrama = new DatagramPacket(almacen, almacen.length, maRe, puerto);
 
+			// enviamos el datagrama
 			miSocket.send(datagrama);
+			
+			// cerramos el datagrama
 			miSocket.close();
+			
+			System.out.println ("Se enviado el mensaje correctamente");
+			
 		}catch (Exception ex){
 			ex.printStackTrace();
 		}
--- src/Ejemplo1Receptor.java
+++ src/Ejemplo1Receptor.java
@@ -6,11 +6,17 @@ public class Ejemplo1Receptor {
 	
 	public static void main (String[] args){
 		
+		
 		System.out.print("Introduce el puerto: \n");
 	 	
+		//Creamos el scaner para recibir el input del usuario
 	 	Scanner sc = new Scanner(System.in);
 	 	
+	 	//creamos la variable puerto de tipo int
 		int puerto = sc.nextInt();
+		
+		// creamos la variable MAX_LON que sera el tamaño del buffer en relacion a
+		// n caracteres
 		
 		final int MAX_LON = 450;
 
@@ -18,14 +24,28 @@ public class Ejemplo1Receptor {
 			
 			System.out.print("Servidor encendido \n");
 		 	
-			
+			// creamos el primer datagram con el puerto como parametro
 			DatagramSocket miSocket = new DatagramSocket(puerto);
+			
+			// creamos el almacenamiento para el mensaje y se le asigna el tamaño
+			// determinado en la variable MAX_LON
+			
 			byte[] almacen = new byte[MAX_LON];
 			
+			// creamos el segundo datagaram con el almacenamiento y tamaño MAX_LON
+			// como parametros
 			DatagramPacket datagrama = new DatagramPacket(almacen, MAX_LON);
+			
+			// recibimos el datamgram en misocket
 			miSocket.receive(datagrama);
+			
+			//asignamos la variable mensaje con el almacenamiento
 			String mensaje = new String(almacen);
+			
+			//imprimimos el mensaje
 			System.out.println(mensaje);
+			
+			//cerramos el socket e imprimimos que el servidor esta apagado
 			miSocket.close();
 			
 			System.out.print("Servidor apagado \n");
